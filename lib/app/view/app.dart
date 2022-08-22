3eb5a6a77e82ab77459ad74ffbfb77f1dcdce340
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-22 09:55:31 -0500

    feat(welcome): define structure

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -18,10 +18,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        appBarTheme: const AppBarTheme(color: Colors.red),
         colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+          accentColor: Colors.red,
         ),
+        primaryColor: Colors.red,
       ),
       localizationsDelegates: const [
         AppLocalizations.delegate,
--- lib/welcome/view/welcome_page.dart
+++ lib/welcome/view/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -12,8 +13,127 @@ class WelcomePage extends StatelessWidget {
 class _WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Poster(
+                  size: size,
+                  posterLength: posters.length,
+                ),
+                const SizedBox(height: 16),
+                _IndexPoster(
+                  index: 0,
+                  posterLength: posters.length,
+                ),
+                const SizedBox(height: 16),
+                _Form(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+class Poster extends StatelessWidget {
+  const Poster({
+    super.key,
+    required this.size,
+    required this.posterLength,
+  });
+
+  final Size size;
+  final int posterLength;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height * 0.75,
+      child: Swiper(
+        itemCount: posterLength,
+        itemBuilder: (context, index) => Container(
+          width: size.width,
+          height: size.height * 0.75,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              12,
+            ),
+            color: Colors.red,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            '${index + 1}',
+            style: TextStyle(fontSize: size.height * 0.7),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _IndexPoster extends StatelessWidget {
+  const _IndexPoster({
+    required this.index,
+    required this.posterLength,
+  });
+
+  final int index;
+  final int posterLength;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget iCircle({required bool isActive}) => Container(
+          width: isActive ? 16 : 8,
+          height: isActive ? 16 : 8,
+          key: UniqueKey(),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: isActive ? Colors.red : Colors.red[200],
+          ),
+        );
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(
+        posterLength,
+        (index) => index == 0
+            ? iCircle(isActive: index == this.index)
+            : Row(
+                children: [
+                  const SizedBox(width: 8),
+                  iCircle(isActive: index == this.index),
+                ],
+              ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          fillColor: Colors.white,
+          hintText: '+ 52 123 456 789',
+          prefixIcon: Icon(Icons.search_rounded),
+          filled: true,
+        ),
+        keyboardType: TextInputType.phone,
+      ),
+    );
+  }
+}
+
+final posters = [1, 2, 3, 4, 5];
