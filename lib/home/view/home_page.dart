1232c2089288c3f8c25bc4e4deb06d06d9a51ea1
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-24 10:57:49 -0500

    feat(home): improve sliver

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -5,140 +5,155 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _HomeView();
+    return const HomeView();
   }
 }
 
-class _HomeView extends StatelessWidget {
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     final scheme = Theme.of(context).colorScheme;
+    final size = MediaQuery.of(context).size;
 
     const avatarSize = 48.0;
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       extendBody: true,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: size.height * 0.3,
-              color: scheme.primary,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Text('Apprecio'),
-                    Container(
-                      height: size.height * 0.4,
-                    )
-                  ],
-                ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            snap: true,
+            stretch: true,
+            backgroundColor: scheme.primary,
+            expandedHeight: size.width,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('Demo'),
+              background: Image.network(
+                'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
+                fit: BoxFit.fitHeight,
+                width: size.width,
+                colorBlendMode: BlendMode.screen,
+                color: scheme.primary,
               ),
             ),
-            Container(
-              height: size.height,
-              width: size.width,
-              color: scheme.onPrimary,
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: size.width,
-                      child: Row(
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                radius: avatarSize,
-                                backgroundColor: scheme.primary,
-                                child: CircleAvatar(
-                                  backgroundColor: scheme.onPrimary,
-                                  radius: avatarSize * 8 / 9,
-                                  child: CircleAvatar(
-                                    radius: (avatarSize * 8 / 9) - 3,
-                                    backgroundImage: const NetworkImage(
-                                      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
-                                    ),
-                                    onBackgroundImageError:
-                                        (exception, stackTrace) =>
-                                            ScaffoldMessenger.of(context)
-                                              ..hideCurrentSnackBar()
-                                              ..showSnackBar(
-                                                const SnackBar(
-                                                  content: Text(
-                                                    'Connect to wifi please',
-                                                  ),
-                                                ),
-                                              ),
-                                    backgroundColor: scheme.onBackground,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                width: avatarSize,
-                                height: avatarSize * 0.4,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  color: scheme.primary,
-                                ),
-                                alignment: Alignment.center,
-                                child: Text(
-                                  '100%',
-                                  style: TextStyle(
-                                    color: scheme.onPrimary,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Text(
-                                  'Hi! Luis',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w900,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: const [
-                                    SizedBox(
-                                      width: 170,
-                                      child: Text(
-                                        'Complete your profile',
-                                        style: TextStyle(fontSize: 16),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverToBoxAdapter(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: avatarSize,
+                        backgroundColor: scheme.primary,
+                        child: CircleAvatar(
+                          backgroundColor: scheme.onPrimary,
+                          radius: avatarSize * 8 / 9,
+                          child: CircleAvatar(
+                            radius: (avatarSize * 8 / 9) - 3,
+                            backgroundImage: const NetworkImage(
+                              'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
+                            ),
+                            onBackgroundImageError: (exception, stackTrace) =>
+                                ScaffoldMessenger.of(context)
+                                  ..hideCurrentSnackBar()
+                                  ..showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                        'Connect to wifi please',
                                       ),
                                     ),
-                                    Icon(
-                                      Icons.arrow_right_rounded,
-                                      size: 50,
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+                                  ),
+                            backgroundColor: scheme.onBackground,
+                          ),
+                        ),
                       ),
+                      Container(
+                        width: avatarSize,
+                        height: avatarSize * 0.4,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: scheme.primary,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '100%',
+                          style: TextStyle(
+                            color: scheme.onPrimary,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          'Hi! Luis',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            SizedBox(
+                              width: 170,
+                              child: Text(
+                                'Complete your profile',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_right_rounded,
+                              size: 50,
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    color: index.isOdd ? Colors.white : Colors.black12,
+                    child: Center(
+                      child: Text('$index', textScaleFactor: 5),
+                    ),
+                  );
+                },
+              ),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
