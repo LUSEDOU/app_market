37cad7480935ce34e320f6ec816f617f92fffe2b
LUSEDOU <dolorier_uribe@hotmail.com>
2022-09-05 09:26:48 -0500

    feat: create navigators

--- lib/app/utils/navigator.dart
+++ lib/app/utils/navigator.dart
@@ -6,6 +6,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppNavigator {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     Route<dynamic> _pageRoute({Widget page = const HomePage()}) {
       return Platform.isIOS
@@ -52,4 +54,46 @@ class AppNavigator {
         return _pageRoute();
     }
   }
+
+  static Future<void> push<T>(Routes route, [T? arguments]) async =>
+      state.pushNamed(AppRoutes.of(route)!, arguments: arguments);
+
+  static Future<void> pushReplacement<T>(Routes route, [T? arguments]) async =>
+      state.pushReplacementNamed(AppRoutes.of(route)!, arguments: arguments);
+
+  static Future<void> pushAndRemoveUntil<T>(
+    Routes route, [
+    T? arguments,
+  ]) async =>
+      state.pushNamedAndRemoveUntil(
+        AppRoutes.of(route)!,
+        (route) => false,
+        arguments: arguments,
+      );
+
+  static Future<void> pop<T>([T? result]) async => state.pop(result);
+
+  static Future<void> popUntil<T>(
+    RoutePredicate predicate, [
+    T? result,
+  ]) async =>
+      state.popUntil(predicate);
+
+  static Future<void> pushNamed<T>(String route, [T? arguments]) async =>
+      state.pushNamed(route, arguments: arguments);
+
+  static Future<void> pushReplacementNamed<T>(
+    String route, [
+    T? arguments,
+  ]) async =>
+      state.pushReplacementNamed(route, arguments: arguments);
+
+  static Future<void> pushNamedAndRemoveUntil<T>(
+    String route,
+    RoutePredicate predicate, [
+    T? arguments,
+  ]) async =>
+      state.pushNamedAndRemoveUntil(route, predicate, arguments: arguments);
+
+  static NavigatorState get state => navigatorKey.currentState!;
 }
